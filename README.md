3e8473bba521e829025f1166fbd31de9f10fb958
GustasHG <145911029+GustasHG@users.noreply.github.com>
2024-12-04 11:16:37 +0000

    adiciona scripts ´lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Clone do portal de noticias do Felipe Dechamps
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,27 @@
 export default function Home() {
-    return(
-        <section>
-            <h1> Isso é pra dar erro, não? </h1>
-            <div>Tabela de test:</div>
-            <table>
-                <thead>
-                    <tr>
-                    <th>Dado 1</th>
-                    <th>Dado 2</th>
-                    <th>Dado 3</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                    <td> Valor (1) </td>
-                    <td> Valor (2) </td>
-                    <td> Valor (3) </td>
-                    </tr>
-                </tbody>
-            </table>
-        </section>
-    )
+  function teste() {
+    console.log("Aoba");
+  }
+  return (
+    <section>
+      <h1> Isso é pra dar erro, não? </h1>
+      <div>Tabela de test:</div>
+      <table>
+        <thead>
+          <tr>
+            <th>Dado 1</th>
+            <th>Dado 2</th>
+            <th>Dado 3</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td> Valor (1) </td>
+            <td> Valor (2) </td>
+            <td> Valor (3) </td>
+          </tr>
+        </tbody>
+      </table>
+    </section>
+  );
 }
