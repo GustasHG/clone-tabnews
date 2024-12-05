07a3b8f1babcdc840cdfdea4d77792d3665ae2ab
GustasHG <145911029+GustasHG@users.noreply.github.com>
2024-12-05 13:03:25 +0000

    add `database.js`and `.env`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,8 @@
 import database from "../../../../infra/database.js";
 
 async function status(req, res) {
-  // const result = await database.query("SELECT 1 + 1");
-  // console.log(result);
+  const result = await database.query("SELECT 1 + 1 as Sum");
+  console.log(result);
   res.status(200).json({ chave: "Alunos do curso.dev são acima da média" });
 }
 
